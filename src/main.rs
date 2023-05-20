d77c4b1aabad6fa0d8c2c8acb87b55b5e6f5d649
Jon Rafkind <jon@rafkind.com>
2023-05-20 15:29:50 -0700

    start to read class file structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,139 @@
+use std::env;
+use std::io::Read;
+
+/*
+ClassFile {
+    u4             magic;
+    u2             minor_version;
+    u2             major_version;
+    u2             constant_pool_count;
+    cp_info        constant_pool[constant_pool_count-1];
+    u2             access_flags;
+    u2             this_class;
+    u2             super_class;
+    u2             interfaces_count;
+    u2             interfaces[interfaces_count];
+    u2             fields_count;
+    field_info     fields[fields_count];
+    u2             methods_count;
+    method_info    methods[methods_count];
+    u2             attributes_count;
+    attribute_info attributes[attributes_count];
+}
+*/
+
+struct ConstantPoolInfo {
+    tag: u8,
+    info: Vec<u8>,
+}
+
+struct FieldInfo {
+    access_flags: u16,
+    name_index: u16,
+    descriptor_index: u16,
+    attributes_count: u16,
+    attributes: Vec<AttributeInfo>,
+}
+
+struct MethodInfo {
+    access_flags: u16,
+    name_index: u16,
+    descriptor_index: u16,
+    attributes_count: u16,
+    attributes: Vec<AttributeInfo>,
+}
+
+struct AttributeInfo {
+    attribute_name_index: u16,
+    attribute_length: u32,
+    info: Vec<u8>,
+}
+
+struct JVMClassFile {
+    magic: u32,
+    minor_version: u16,
+    major_version: u16,
+    constant_pool_count: u16,
+    constant_pool: Vec<ConstantPoolInfo>,
+    access_flags: u16,
+    this_class: u16,
+    super_class: u16,
+    interfaces_count: u16,
+    interfaces: Vec<u16>,
+    fields_count: u16,
+    fields: Vec<FieldInfo>,
+    methods_count: u16,
+    methods: Vec<MethodInfo>,
+    attributes_count: u16,
+    attributes: Vec<AttributeInfo>,
+}
+
+fn read_u32_bigendian(file: &mut std::fs::File) -> u32 {
+    let mut buf = [0; 4];
+    file.read_exact(&mut buf).unwrap();
+    u32::from_be_bytes(buf)
+}
+
+fn read_u16_bigendian(file: &mut std::fs::File) -> u16 {
+    let mut buf = [0; 2];
+    file.read_exact(&mut buf).unwrap();
+    u16::from_be_bytes(buf)
+}
+
+// jvm class specification
+// https://docs.oracle.com/javase/specs/jvms/se20/html/jvms-4.html
+fn parse_class_file(filename: &str) {
+    println!("Parsing file: {}", filename);
+    // open filename as binary
+
+    match std::fs::File::open(filename) {
+        Ok(file) => {
+            let mut file = file;
+            let mut jvm_class_file = JVMClassFile {
+                magic: 0,
+                minor_version: 0,
+                major_version: 0,
+                constant_pool_count: 0,
+                constant_pool: Vec::new(),
+                access_flags: 0,
+                this_class: 0,
+                super_class: 0,
+                interfaces_count: 0,
+                interfaces: Vec::new(),
+                fields_count: 0,
+                fields: Vec::new(),
+                methods_count: 0,
+                methods: Vec::new(),
+                attributes_count: 0,
+                attributes: Vec::new(),
+            };
+            jvm_class_file.magic = read_u32_bigendian(&mut file);
+            jvm_class_file.minor_version = read_u16_bigendian(&mut file);
+            jvm_class_file.major_version = read_u16_bigendian(&mut file);
+            jvm_class_file.constant_pool_count = read_u16_bigendian(&mut file);
+
+            jvm_class_file.access_flags = read_u16_bigendian(&mut file);
+            jvm_class_file.this_class = read_u16_bigendian(&mut file);
+            jvm_class_file.super_class = read_u16_bigendian(&mut file);
+            jvm_class_file.interfaces_count = read_u16_bigendian(&mut file);
+
+            println!("Magic: 0x{0:x}", jvm_class_file.magic);
+            println!("Version: {0}.{1}", jvm_class_file.major_version, jvm_class_file.minor_version);
+        },
+        Err(error) => println!("Error opening file: {error}")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // iterate through arguments and print each one out
+    /*
+    for arg in args.iter() {
+        println!("{}", arg);
+    }
+    */
+    // print just the first argument out, but only if there is at least one argument
+    if args.len() > 1 {
+        parse_class_file(args[1].as_str());
+    }
 }
