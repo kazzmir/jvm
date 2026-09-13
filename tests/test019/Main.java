79d8691129a609cc31e97d52187014e0eb58add5
Jon Rafkind <jon@rafkind.com>
2026-09-13 21:10:28 +0000

    add test19 test

---
+++ tests/test019/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String... args) {
+        float[] data = new float[3];
+        data[0] = 1.0f;
+        data[1] = 2.0f;
+
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
+        }
+    }
+}
